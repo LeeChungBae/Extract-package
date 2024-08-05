780dfc09a364e9e9a2105aae28ed02a10b29050c
NishNovae <raecrowned@gmail.com>
2024-08-05 10:27:19 +0900

    extract.py

--- src/extract_package/extract.py
+++ src/extract_package/extract.py
@@ -10,9 +10,20 @@ def get_key():
     key = os.getenv("MOVIE_API_KEY")        # need to add how-to in README
     return key
 
-def gen_url(dt="20120101", url_param={}):
+def gen_url(dt="20120101"):
     base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
     key = get_key()
     url = f"{base_url}?key={key}&targetDt={dt}"
 
-## continue next tim1!!!
+def request(dt="20120101"):
+    url = gen_url(dt)
+    r = requests.get(url)
+    data = r.json()
+    return data
+
+def save_pq(dt="20120101", parq_path=SAVE_PATH)
+    data = request(dt)
+    data_list = data['boxOfficeResult']['dailyBoxOfficeList']
+    df = pd.DataFrame(data_list)
+    df.to_parquet(parq_path, partition_cols=['load_dt'])
+    return df
